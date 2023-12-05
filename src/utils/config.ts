21c55d24266ee3c251d689d3a1e33d0705f8179c
Juhani Juusola <juhani.juusola@gmail.com>
2023-12-05 20:40:39 +0200

    feat: added missing witnesses testimonials (en)

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -78,7 +78,7 @@ const config = yaml.load(fs.readFileSync('src/config.yaml', 'utf8')) as {
   analytics?: unknown;
 };
 
-const DEFAULT_SITE_NAME = 'https://flamethefreeze.com/';
+const DEFAULT_SITE_NAME = 'WEBSITE';
 
 const getSite = () => {
   const _default = {
