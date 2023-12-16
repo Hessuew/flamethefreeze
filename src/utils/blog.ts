e8e311d13507b1426d0eed31f7739cbda30e34f4
Juhani Juusola <juhani.juusola@gmail.com>
2023-12-16 13:08:19 +0200

    feat: Biome support, reworking witnesses by the Spirit of God

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -1,103 +1,114 @@
-import { getCollection } from 'astro:content';
-import type { CollectionEntry } from 'astro:content';
-import type { Post } from '~/types';
-import { APP_BLOG } from '~/utils/config';
-import { cleanSlug, trimSlash, BLOG_BASE, POST_PERMALINK_PATTERN, CATEGORY_BASE, TAG_BASE } from './permalinks';
+import type { Post } from "~/types";
+import { APP_BLOG } from "~/utils/config";
+import {
+	BLOG_BASE,
+	CATEGORY_BASE,
+	POST_PERMALINK_PATTERN,
+	TAG_BASE,
+	cleanSlug,
+	trimSlash,
+} from "./permalinks";
+import { getCollection } from "astro:content";
+import type { CollectionEntry } from "astro:content";
 
 const generatePermalink = async ({
-  id,
-  slug,
-  publishDate,
-  category,
+	id,
+	slug,
+	publishDate,
+	category,
 }: {
-  id: string;
-  slug: string;
-  publishDate: Date;
-  category: string | undefined;
+	id: string;
+	slug: string;
+	publishDate: Date;
+	category: string | undefined;
 }) => {
-  const year = String(publishDate.getFullYear()).padStart(4, '0');
-  const month = String(publishDate.getMonth() + 1).padStart(2, '0');
-  const day = String(publishDate.getDate()).padStart(2, '0');
-  const hour = String(publishDate.getHours()).padStart(2, '0');
-  const minute = String(publishDate.getMinutes()).padStart(2, '0');
-  const second = String(publishDate.getSeconds()).padStart(2, '0');
+	const year = String(publishDate.getFullYear()).padStart(4, "0");
+	const month = String(publishDate.getMonth() + 1).padStart(2, "0");
+	const day = String(publishDate.getDate()).padStart(2, "0");
+	const hour = String(publishDate.getHours()).padStart(2, "0");
+	const minute = String(publishDate.getMinutes()).padStart(2, "0");
+	const second = String(publishDate.getSeconds()).padStart(2, "0");
 
-  const permalink = POST_PERMALINK_PATTERN.replace('%slug%', slug)
-    .replace('%id%', id)
-    .replace('%category%', category || '')
-    .replace('%year%', year)
-    .replace('%month%', month)
-    .replace('%day%', day)
-    .replace('%hour%', hour)
-    .replace('%minute%', minute)
-    .replace('%second%', second);
+	const permalink = POST_PERMALINK_PATTERN.replace("%slug%", slug)
+		.replace("%id%", id)
+		.replace("%category%", category || "")
+		.replace("%year%", year)
+		.replace("%month%", month)
+		.replace("%day%", day)
+		.replace("%hour%", hour)
+		.replace("%minute%", minute)
+		.replace("%second%", second);
 
-  return permalink
-    .split('/')
-    .map((el) => trimSlash(el))
-    .filter((el) => !!el)
-    .join('/');
+	return permalink
+		.split("/")
+		.map((el) => trimSlash(el))
+		.filter((el) => !!el)
+		.join("/");
 };
 
-const getNormalizedPost = async (post: CollectionEntry<'post'>): Promise<Post> => {
-  const { id, slug: rawSlug = '', data } = post;
-  const { Content, remarkPluginFrontmatter } = await post.render();
+const getNormalizedPost = async (
+	post: CollectionEntry<"post">,
+): Promise<Post> => {
+	const { id, slug: rawSlug = "", data } = post;
+	const { Content, remarkPluginFrontmatter } = await post.render();
 
-  const {
-    publishDate: rawPublishDate = new Date(),
-    updateDate: rawUpdateDate,
-    title,
-    excerpt,
-    image,
-    tags: rawTags = [],
-    category: rawCategory,
-    author,
-    draft = false,
-    metadata = {},
-  } = data;
+	const {
+		publishDate: rawPublishDate = new Date(),
+		updateDate: rawUpdateDate,
+		title,
+		excerpt,
+		image,
+		tags: rawTags = [],
+		category: rawCategory,
+		author,
+		draft = false,
+		metadata = {},
+	} = data;
 
-  const slug = cleanSlug(rawSlug); // cleanSlug(rawSlug.split('/').pop());
-  const publishDate = new Date(rawPublishDate);
-  const updateDate = rawUpdateDate ? new Date(rawUpdateDate) : undefined;
-  const category = rawCategory ? cleanSlug(rawCategory) : undefined;
-  const tags = rawTags.map((tag: string) => cleanSlug(tag));
+	const slug = cleanSlug(rawSlug); // cleanSlug(rawSlug.split('/').pop());
+	const publishDate = new Date(rawPublishDate);
+	const updateDate = rawUpdateDate ? new Date(rawUpdateDate) : undefined;
+	const category = rawCategory ? cleanSlug(rawCategory) : undefined;
+	const tags = rawTags.map((tag: string) => cleanSlug(tag));
 
-  return {
-    id: id,
-    slug: slug,
-    permalink: await generatePermalink({ id, slug, publishDate, category }),
+	return {
+		id: id,
+		slug: slug,
+		permalink: await generatePermalink({ id, slug, publishDate, category }),
 
-    publishDate: publishDate,
-    updateDate: updateDate,
+		publishDate: publishDate,
+		updateDate: updateDate,
 
-    title: title,
-    excerpt: excerpt,
-    image: image,
+		title: title,
+		excerpt: excerpt,
+		image: image,
 
-    category: category,
-    tags: tags,
-    author: author,
+		category: category,
+		tags: tags,
+		author: author,
 
-    draft: draft,
+		draft: draft,
 
-    metadata,
+		metadata,
 
-    Content: Content,
-    // or 'content' in case you consume from API
+		Content: Content,
+		// or 'content' in case you consume from API
 
-    readingTime: remarkPluginFrontmatter?.readingTime,
-  };
+		readingTime: remarkPluginFrontmatter?.readingTime,
+	};
 };
 
-const load = async function (): Promise<Array<Post>> {
-  const posts = await getCollection('post');
-  const normalizedPosts = posts.map(async (post) => await getNormalizedPost(post));
+const load = async (): Promise<Array<Post>> => {
+	const posts = await getCollection("post");
+	const normalizedPosts = posts.map(
+		async (post) => await getNormalizedPost(post),
+	);
 
-  const results = (await Promise.all(normalizedPosts))
-    .sort((a, b) => b.publishDate.valueOf() - a.publishDate.valueOf())
-    .filter((post) => !post.draft);
+	const results = (await Promise.all(normalizedPosts))
+		.sort((a, b) => b.publishDate.valueOf() - a.publishDate.valueOf())
+		.filter((post) => !post.draft);
 
-  return results;
+	return results;
 };
 
 let _posts: Array<Post>;
@@ -118,109 +129,121 @@ export const blogPostsPerPage = APP_BLOG?.postsPerPage;
 
 /** */
 export const fetchPosts = async (): Promise<Array<Post>> => {
-  if (!_posts) {
-    _posts = await load();
-  }
+	if (!_posts) {
+		_posts = await load();
+	}
 
-  return _posts;
+	return _posts;
 };
 
 /** */
-export const findPostsBySlugs = async (slugs: Array<string>): Promise<Array<Post>> => {
-  if (!Array.isArray(slugs)) return [];
+export const findPostsBySlugs = async (
+	slugs: Array<string>,
+): Promise<Array<Post>> => {
+	if (!Array.isArray(slugs)) return [];
 
-  const posts = await fetchPosts();
+	const posts = await fetchPosts();
 
-  return slugs.reduce(function (r: Array<Post>, slug: string) {
-    posts.some(function (post: Post) {
-      return slug === post.slug && r.push(post);
-    });
-    return r;
-  }, []);
+	return slugs.reduce((r: Array<Post>, slug: string) => {
+		posts.some((post: Post) => slug === post.slug && r.push(post));
+		return r;
+	}, []);
 };
 
 /** */
-export const findPostsByIds = async (ids: Array<string>): Promise<Array<Post>> => {
-  if (!Array.isArray(ids)) return [];
+export const findPostsByIds = async (
+	ids: Array<string>,
+): Promise<Array<Post>> => {
+	if (!Array.isArray(ids)) return [];
 
-  const posts = await fetchPosts();
+	const posts = await fetchPosts();
 
-  return ids.reduce(function (r: Array<Post>, id: string) {
-    posts.some(function (post: Post) {
-      return id === post.id && r.push(post);
-    });
-    return r;
-  }, []);
+	return ids.reduce((r: Array<Post>, id: string) => {
+		posts.some((post: Post) => id === post.id && r.push(post));
+		return r;
+	}, []);
 };
 
 /** */
-export const findLatestPosts = async ({ count }: { count?: number }): Promise<Array<Post>> => {
-  const _count = count || 4;
-  const posts = await fetchPosts();
+export const findLatestPosts = async ({
+	count,
+}: { count?: number }): Promise<Array<Post>> => {
+	const _count = count || 4;
+	const posts = await fetchPosts();
 
-  return posts ? posts.slice(0, _count) : [];
+	return posts ? posts.slice(0, _count) : [];
 };
 
 /** */
 export const getStaticPathsBlogList = async ({ paginate }) => {
-  if (!isBlogEnabled || !isBlogListRouteEnabled) return [];
-  return paginate(await fetchPosts(), {
-    params: { blog: BLOG_BASE || undefined },
-    pageSize: blogPostsPerPage,
-  });
+	if (!isBlogEnabled || !isBlogListRouteEnabled) return [];
+	return paginate(await fetchPosts(), {
+		params: { blog: BLOG_BASE || undefined },
+		pageSize: blogPostsPerPage,
+	});
 };
 
 /** */
 export const getStaticPathsBlogPost = async () => {
-  if (!isBlogEnabled || !isBlogPostRouteEnabled) return [];
-  return (await fetchPosts()).flatMap((post) => ({
-    params: {
-      blog: post.permalink,
-    },
-    props: { post },
-  }));
+	if (!isBlogEnabled || !isBlogPostRouteEnabled) return [];
+	return (await fetchPosts()).flatMap((post) => ({
+		params: {
+			blog: post.permalink,
+		},
+		props: { post },
+	}));
 };
 
 /** */
 export const getStaticPathsBlogCategory = async ({ paginate }) => {
-  if (!isBlogEnabled || !isBlogCategoryRouteEnabled) return [];
+	if (!isBlogEnabled || !isBlogCategoryRouteEnabled) return [];
 
-  const posts = await fetchPosts();
-  const categories = new Set();
-  posts.map((post) => {
-    typeof post.category === 'string' && categories.add(post.category.toLowerCase());
-  });
+	const posts = await fetchPosts();
+	const categories = new Set();
+	posts.map((post) => {
+		typeof post.category === "string" &&
+			categories.add(post.category.toLowerCase());
+	});
 
-  return Array.from(categories).flatMap((category: string) =>
-    paginate(
-      posts.filter((post) => typeof post.category === 'string' && category === post.category.toLowerCase()),
-      {
-        params: { category: category, blog: CATEGORY_BASE || undefined },
-        pageSize: blogPostsPerPage,
-        props: { category },
-      }
-    )
-  );
+	return Array.from(categories).flatMap((category: string) =>
+		paginate(
+			posts.filter(
+				(post) =>
+					typeof post.category === "string" &&
+					category === post.category.toLowerCase(),
+			),
+			{
+				params: { category: category, blog: CATEGORY_BASE || undefined },
+				pageSize: blogPostsPerPage,
+				props: { category },
+			},
+		),
+	);
 };
 
 /** */
 export const getStaticPathsBlogTag = async ({ paginate }) => {
-  if (!isBlogEnabled || !isBlogTagRouteEnabled) return [];
+	if (!isBlogEnabled || !isBlogTagRouteEnabled) return [];
 
-  const posts = await fetchPosts();
-  const tags = new Set();
-  posts.map((post) => {
-    Array.isArray(post.tags) && post.tags.map((tag) => tags.add(tag.toLowerCase()));
-  });
+	const posts = await fetchPosts();
+	const tags = new Set();
+	posts.map((post) => {
+		Array.isArray(post.tags) &&
+			post.tags.map((tag) => tags.add(tag.toLowerCase()));
+	});
 
-  return Array.from(tags).flatMap((tag: string) =>
-    paginate(
-      posts.filter((post) => Array.isArray(post.tags) && post.tags.find((elem) => elem.toLowerCase() === tag)),
-      {
-        params: { tag: tag, blog: TAG_BASE || undefined },
-        pageSize: blogPostsPerPage,
-        props: { tag },
-      }
-    )
-  );
+	return Array.from(tags).flatMap((tag: string) =>
+		paginate(
+			posts.filter(
+				(post) =>
+					Array.isArray(post.tags) &&
+					post.tags.find((elem) => elem.toLowerCase() === tag),
+			),
+			{
+				params: { tag: tag, blog: TAG_BASE || undefined },
+				pageSize: blogPostsPerPage,
+				props: { tag },
+			},
+		),
+	);
 };
